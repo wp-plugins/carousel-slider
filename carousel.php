844233e9d85fea1c1465e82642bf2b89fa49da85
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-07 15:09:51 +0000

    adding new file
    git-svn-id: https://plugins.svn.wordpress.org/carousel-slider/trunk@983694 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- carousel.php
+++ carousel.php
@@ -3,7 +3,7 @@
 Plugin Name: Carousel Slider
 Plugin URI: http://wordpress.org/plugins/carousel-slider
 Description: Touch enabled wordpress plugin that lets you create beautiful responsive carousel slider.
-Version: 1.1
+Version: 1.2
 Author: Sayful Islam
 Author URI: http://sayful.net
 Text Domain: carouselslider
